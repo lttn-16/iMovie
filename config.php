7ba900bc0a331f40fe5829dfbb6e3c2f4e051704
Nhi Hồ <honhi07012000@gmail.com>
2021-06-15 17:39:56 +0700

    update

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 	$gClient = new Google_Client();
 	$gClient->setClientId("809386260979-de8sv7qioa5224rhcgp5tq97mav7k3aq.apps.googleusercontent.com");
 	$gClient->setClientSecret("at83eWkyGcZD_Pzu3qKaApnV");
-	$gClient->setApplicationName("CPI Login Tutorial");
+	$gClient->setApplicationName("iMovie");
 	$gClient->setRedirectUri("http://localhost/iMovie/g-callback.php");
 	$gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
 ?>
