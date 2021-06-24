12f0c2ce1541930abea31f3bcad40ca580d14c40
= <=>
2021-06-24 22:34:44 +0700

    change db

--- include/db.php
+++ include/db.php
@@ -1,6 +1,7 @@
 <?php
-$db['db_host'] = 'local';
-$connection = mysqli_connect('localhost', 'root', '', 'imovie');
+//$db['db_host'] = 'local';
+//$connection = mysqli_connect('localhost', 'root', '', 'imovie');
+$connection = mysqli_connect('sql5.freemysqlhosting.net', 'sql5421214', 'GME43ciZIl', 'sql5421214');
 //check connect
 // if($connection){
 //     echo "yes connected!";
