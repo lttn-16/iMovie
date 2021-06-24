ad91de04c20c9fbd2f9f3648fe02aa6da72f5161
= <=>
2021-06-24 22:50:03 +0700

    fix charset

--- include/db.php
+++ include/db.php
@@ -1,7 +1,9 @@
 <?php
 //$db['db_host'] = 'local';
 //$connection = mysqli_connect('localhost', 'root', '', 'imovie');
+header("Content-type: text/html; charset=utf-8");
 $connection = mysqli_connect('sql5.freemysqlhosting.net', 'sql5421214', 'GME43ciZIl', 'sql5421214');
+mysqli_set_charset($connection, 'UTF8');
 //check connect
 // if($connection){
 //     echo "yes connected!";
