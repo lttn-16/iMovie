50a7cee38060fdd9e3a314d41b72bc7fbeac2fc1
= <=>
2021-06-25 21:31:46 +0700

    new branch

--- include/db.php
+++ include/db.php
@@ -1,8 +1,8 @@
 <?php
 
 header("Content-type: text/html; charset=utf-8");
-$connection = mysqli_connect('sql5.freemysqlhosting.net', 'sql5421214', 'GME43ciZIl', 'sql5421214');
-//$connection = mysqli_connect('localhost', 'root', '', 'imovie');
+//$connection = mysqli_connect('sql5.freemysqlhosting.net', 'sql5421214', 'GME43ciZIl', 'sql5421214');
+$connection = mysqli_connect('localhost', 'root', '', 'imovie');
 mysqli_set_charset($connection, 'UTF8');
 
 //check connect
